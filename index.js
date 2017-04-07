47242feac5378958cffd00634acb2009f81ca1f2
Tommaso Soprana <tommaso.soprana@ynap.com>
2017-04-07 16:07:52 +0200

    From csv to zip
    From csv to zip

--- index.js
+++ index.js
@@ -25,63 +25,97 @@ var visual_recognition = watson.visual_recognition({
 
 var download = function(uri, filename, callback){
   request.head(uri, function(err, res, body){
-    console.log(filename+' ('+res.headers['content-type']+') ' + res.headers['content-length']);
+    //console.log(filename+' ('+res.headers['content-type']+') ' + res.headers['content-length']);
     request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
   });
 };
 
 var rmDir = function(dirPath) {
-	try { var files = fs.readdirSync(dirPath); }
-	catch(e) { return; }
-	if (files.length > 0)
-	for (var i = 0; i < files.length; i++) {
-	  var filePath = dirPath + '/' + files[i];
-	  if (fs.statSync(filePath).isFile())
-	    fs.unlinkSync(filePath);
-	  else
-	    rmDir(filePath);
-	}
-};
+  try { var files = fs.readdirSync(dirPath); }
+  catch(e) { return; }
+  if (files.length > 0)
+    for (var i = 0; i < files.length; i++) {
+      var filePath = dirPath + '/' + files[i];
+      if (fs.statSync(filePath).isFile())
+        fs.unlinkSync(filePath);
+      else
+        rmDir(filePath);
+        //fs.rmdirSync(filePath);
+    }
+  };
 
-var zipIt = function() {
-	var output = fs.createWriteStream('final.zip');
+var zipIt = function(sourcePath, savePath) {
+	var output = fs.createWriteStream(savePath);
 	var archive = archiver('zip');
 
+  console.log(sourcePath);
+  
 	output.on('close', function () {
 	    console.log("Zip done. " + archive.pointer() + ' bytes');
 	});
-
-	archive.on('error', function(err){
-	    throw err;
-	});
-
+  
+	archive.on('error', function(err) { throw err; });
 	archive.pipe(output);
-	archive.bulk([
-	    { expand: true, cwd: 'pool', src: ['**'], dest: ''}
-	]);
+	//archive.directory(sourcePath);
+  archive.bulk([{expand: true, cwd: sourcePath, src: ['**']}]);
 	archive.finalize();
 };
 
 
 /* CODE *******************************/
 
-rmDir("./pool/");
+// Inizializzazione con pulizia della pool dei dati
+//rmDir("./pool/");
+rmDir("./zip/");
+var rawData = {};
 
+// Retrieve initial csv file
 csv()
 .fromFile(csvFilePath)
 .on('csv',(csvRow, rowIndex)=>{
-    	counter ++;
+  var classification = csvRow[1];
+  var image = csvRow[0]+'.jpg';
+
+  // riorganizzo i i dati in base alla classificazione
+  if (typeof rawData[classification] == "undefined") rawData[classification] = [];
+  rawData[classification].push(image);
+
+  //console.log(classification, image);
+  //rawData[classification].push(image);
+  /*
+  counter ++;
 	if (counter <= counterLimit) {
 		var imageName = csvRow[0]+'.jpg';
 		download(baseUrlImg+imageName, "./pool/"+imageName, function(){
   			//console.log(rowIndex, imageName+' ..done');
 		});
 	}
-    	//else console.log(jsonObj)
+  */
 })
 .on('done',(error)=>{
-	zipIt();
-    	console.log('end')
+
+  //zipIt('zip/final.zip');
+  //console.log(rawData);
+
+  for (var k in rawData){
+    var dir = './pool/'+k;
+
+    if (!fs.existsSync(dir)){
+        fs.mkdirSync(dir);
+    }
+    counter = 0;
+
+    for (var kk in rawData[k]){
+      var imageName = rawData[k][kk];
+      counter ++;
+      if (counter <= counterLimit) {
+        //console.log(kk, baseUrlImg+imageName, "./pool/"+k+"/"+imageName);
+        download(baseUrlImg+imageName, "./pool/"+k+"/"+imageName, function(){});
+      }
+    }
+    zipIt('pool/'+k, 'zip/'+k+'_positive_examples.zip');
+  }
+  console.log('end')
 })
 
 
