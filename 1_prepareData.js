8f331c54917565b1310dc2ecabd579e8af7cf3a2
Tommaso Soprana <tommaso.soprana@ynap.com>
2017-04-10 09:35:32 +0200

    Comments

--- 1_prepareData.js
+++ 1_prepareData.js
@@ -15,6 +15,9 @@ var fs = require('fs'),
 
 
 /* METHODS ****************************/
+// in this section you can find all methods i've used for the code below
+
+// this method remove all files and subdirectories inside a specific directory
 var rmDir = function(dirPath) {
   try { var files = fs.readdirSync(dirPath); }
   catch(e) { return; }
@@ -29,6 +32,7 @@ var rmDir = function(dirPath) {
     }
   };
 
+// this method create a zip file containing all files inside a directory
 var zipIt = function(sourcePath, savePath) {
 	var output = fs.createWriteStream(savePath);
 	var archive = archiver('zip');
@@ -42,6 +46,7 @@ var zipIt = function(sourcePath, savePath) {
 	archive.finalize();
 };
 
+// this method reshuffle randomly an array
 var shuffleArray = function(array) {
     for (var i = array.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
@@ -54,7 +59,7 @@ var shuffleArray = function(array) {
 
 
 /* CODE *******************************/
-// Inizializzazione con pulizia della pool dei dati
+// Before starting we need to clean all the previously data (if not empty)
 rmDir("./pool/");
 rmDir("./zip/");
 var rawData = {};
@@ -63,20 +68,22 @@ var rawData = {};
 csv()
 .fromFile(csvFilePath)
 .on('csv',(csvRow, rowIndex)=>{
+  // watson need the classification not to have special chars so i will remove them 
   var classification = csvRow[1].replace(/[^a-zA-Z ]/g, "");
   var image = csvRow[0]+'.jpg';
 
-  // riorganizzo i dati in base alla classificazione
+  // populate the rawData array with all the information in the csv but rearranged
+  // based on classification
   if (typeof rawData[classification] == "undefined") rawData[classification] = [];
   rawData[classification].push(image);
 })
 .on('done',(error)=>{
+  // when the rawData array is populated we walk on it
   for (var k in rawData) {
-    
+    // the specific classification is randomly reordered
     var shuffledData = shuffleArray(rawData[k]);
-
+    // then il prepare its directory (to save inside all images)
     var dir = './pool/'+k;
-
     if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir);
     }
@@ -99,7 +106,8 @@ csv()
           asyncFunction(item, resolve);
         });
     })
-
+    // i use the promises to download each imagge in its directory
+    // when all images are done, for every classification (category) i generate a zip file.
     Promise.all(requests).then(res => {
       var category = res[0];
       zipIt('pool/'+category, 'zip/'+category+'.zip');
